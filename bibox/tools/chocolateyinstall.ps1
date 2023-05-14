d8a533c1d56374cf1e867cec68aee65154a9f34e
Trinity503 <54348970+Trinity503@users.noreply.github.com>
2023-05-14 09:33:02 +0200

    initial

---
+++ bibox/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = 'https://static.bibox2.westermann.de/electron/autoUpdate//BiBox%202.0%202.4.4.msi'
+  #https://static.bibox2.westermann.de/electron/autoUpdate/BiBox%202.0%202.4.4.msi
+
+  softwareName  = 'bibox'
+
+  checksum      = ''
+  checksumType  = ''
+  checksum64    = '78E4FBDA66376CD529E83F6667D65A94C6490F2C189D340AC42D5D29261242E2'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn ALLUSERS=1 /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
